839b5e47c85bde63c41d647b2ade261e304282a5
Madhuvanthi-A <madhu@servelots.com>
2019-04-23 11:42:29 +0530

    added image url and tags to respective time stamp

--- maaya-oh/resources.js
+++ maaya-oh/resources.js
@@ -87,5 +87,149 @@ window.resources = [{
 		"url": "http://team.servelots.com/my/pradeep/girijakalyana/narrators/narrators2.JPG"
 	}
 	]
+},
+{
+	"start": "06:10",
+	"end": "07:57",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Engagement of Shiva and Parvati"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/Marriage-engagement.JPG"
+	}
+	]
+}
+,
+{
+	"start": "07:58",
+	"end": "12:49",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Girija Kalyana"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/IMG_3720_Kalyanotsava.jpg"
+	}
+	]
+},
+{
+	"start": "13:58",
+	"end": "14:36",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Girija Kalyana"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/IMG_3709_Kalyanotsava.jpg"
+	}
+	]
+},
+{
+	"start": "14:37",
+	"end": "16:56",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Week long celebration - Rathotsava"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/Rathostava/DSCN4333.JPG"
+	}
+	]
+},
+{
+	"start": "16:57",
+	"end": "17:21",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Week long celebration - Rathotsava"],
+		"url": "http://gk.chaha.in/static/uploads/DSC02396.JPG"
+	}
+	]
+},
+{
+	"start": "17:22",
+	"end": "17:34",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Girija Kalyana - Teppotsava"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/Teppotsava/DSC_4010.JPG"
+	}
+	]
+},
+{
+	"start": "17:35",
+	"end": "18:10",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Paintings depicting the Girija Kalyana scene"],
+		"url": "http://www.artnewsnviews.com/userfiles/image/sep-11/antique-05.png"
+	}
+	]
+},
+{
+	"start": "18:11",
+	"end": "19:07",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Virupaksha temple-Hampi"],
+		"url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Virupaksha_Temple_in_Hampi.jpg"
+	}
+	]
+},
+{
+	"start": "19:08",
+	"end": "22:59",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Collection of ancient texts"],
+		"url": "https://itihasaacademy.files.wordpress.com/2013/08/copper-plate.jpg"
+	}
+	]
+},
+{
+	"start": "23:00",
+	"end": "24:41",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Shiva's meditation"],
+		"url": "https://i.pinimg.com/originals/d0/fe/3f/d0fe3fd2e3a455015b2343d05e2bf7c8.jpg"
+	}
+	]
+},
+{
+	"start": "24:40",
+	"end": "25:31",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["Girija Kalyana"],
+		"url": "https://3.bp.blogspot.com/-0CVbvYg5LxA/XJMvvV1JblI/AAAAAAAAs4I/M2Oy13A4eCww9Ls2OAKmC9kH3aIdyvPZACLcBGAs/s1600/800px-Deity_sculpture_in_Srikanteshwara_temple_at_Nanjangud_2%2Bgirijakalyana%2BMoorty.JPG"
+	}
+	]
+},
+{
+	"start": "25:33",
+	"end": "27:11",
+	"resources": [
+	{
+		"type": "image",
+		"tags": ["History of Hampi and it's heritage"],
+		"url": "http://team.servelots.com/my/pradeep/chaluvaraju/IMG_3710_Kalyanotsava.jpg"
+	}
+	]
+},
+{
+	"start": "",
+	"end": "",
+	"resources": [
+	{
+		"type": "image",
+		"tags": [""],
+		"url": ""
+	}
+	]
 }
 ]
